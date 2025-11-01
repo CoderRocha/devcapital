bd99ad82a2cd85372f02bb77a0543de6fcfed249
Guilherme <guilhermerochas0420@gmail.com>
2025-11-01 14:38:39 -0300

    chore: added shadcn ui

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+export default {
+    content: ['./src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+      extend: {
+        colors: {
+          primary: {
+            DEFAULT: '#1E3A8A',
+            foreground: '#FFFFFF',
+          },
+          secondary: {
+            DEFAULT: '#3B82F6',
+            foreground: '#FFFFFF',
+          },
+        },
+      },
+    },
+    plugins: [],
+};
