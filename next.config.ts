a3381e9ee5d146b64d437db686060660ca3e8da4
Guilherme <guilhermerochas0420@gmail.com>
2025-12-22 19:29:04 -0300

    chore: updated next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // remover essa parte para rodar localmente
+  output: 'export',
+  basePath: '/dev-capital',
+  images: {
+  unoptimized: true,
+  },
 };
 
 export default nextConfig;
